242bd4f286e19db860886a710b616a16241b9c13
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2026-08-10 15:44:14 +0200

    a

--- frontend/src/lib/prompts/form.ts
+++ frontend/src/lib/prompts/form.ts
@@ -1,71 +1,69 @@
-import type { Prettify } from "@chocbite/ts-lib-common";
-import form, {
-  FormElement,
-  FormValue,
-  type FormGroup,
-} from "@chocbite/ts-lib-form";
-import { type Option } from "@chocbite/ts-lib-result";
-import { main_panel_container } from "@libComposition";
-import { Prompt } from "./shared";
+import { sleep, type Prettify } from '@chocbite/ts-lib-common';
+import form, { FormElement, FormValue, type FormGroup } from '@chocbite/ts-lib-form';
+import { type Option } from '@chocbite/ts-lib-result';
+import { main_panel_container } from '@libComposition';
+import { Prompt } from './shared';
 
-type GroupExtractVals<Arr extends any[]> = Arr extends [
-  infer Head,
-  ...infer Tail,
-]
-  ? Head extends FormValue<infer T, infer ID>
-    ? [FormValue<T, ID>, ...GroupExtractVals<Tail>]
-    : [...GroupExtractVals<Tail>]
-  : [];
+type GroupExtractVals<Arr extends any[]> = Arr extends [infer Head, ...infer Tail]
+	? Head extends FormValue<infer T, infer ID>
+		? [FormValue<T, ID>, ...GroupExtractVals<Tail>]
+		: [...GroupExtractVals<Tail>]
+	: [];
 type GroupToKeyVal<Arr extends FormValue<any, any>[]> = {
-  [K in Arr[number] as K["form_id"]]: K extends FormValue<infer T, any>
-    ? T
-    : never;
+	[K in Arr[number] as K['form_id']]: K extends FormValue<infer T, any> ? T : never;
 };
 
 interface Submit<RT extends object> {
-  text: string;
-  click?: (group: FormGroup<RT, any>) => void;
+	text: string;
+	click?: (group: FormGroup<RT, any>) => void;
 }
 
 export function prompt_form<
-  L extends FormElement[],
-  T extends object = Prettify<Partial<GroupToKeyVal<GroupExtractVals<L>>>>,
->(
-  text: string,
-  elements: [...L],
-  submit: Submit<T>,
-): Promise<Option<Partial<T>>> {
-  return new Promise<Option<Partial<T>>>((resolve) => {
-    const group = form.group<L, undefined, T>({
-      elements,
-      embed: true,
-      column: "6rem",
-    });
-    const prompt = new Prompt(
-      text,
-      form.group({
-        elements: [
-          group,
-          form.button({
-            text: submit.text,
-            on_click: () => {
-              const values = group.value_partial;
-              submit.click?.(group);
-              prompt.content_close();
-              resolve(values.to_option());
-            },
-          }),
-        ],
-      }),
-    );
-    main_panel_container
-      .create_panel(prompt, {
-        sizeable: false,
-        moveable: false,
-        modal: true,
-        width: 20,
-      })
-      .on_close()
-      .then(resolve);
-  });
+	L extends FormElement[],
+	T extends object = Prettify<Partial<GroupToKeyVal<GroupExtractVals<L>>>>,
+>(text: string, elements: [...L], submit: Submit<T>): Promise<Option<Partial<T>>> {
+	return new Promise<Option<Partial<T>>>((resolve) => {
+		const group = form.group<L, undefined, T>({
+			elements,
+			embed: true,
+			column: '6rem',
+		});
+		const prompt = new Prompt(
+			text,
+			form.group({
+				elements: [
+					group,
+					form.button({
+						text: submit.text,
+						on_click: () => {
+							const values = group.value_partial;
+							submit.click?.(group);
+							prompt.content_close();
+							resolve(values.to_option());
+						},
+						center: true,
+					}),
+				],
+			}),
+			async (e) => {
+				if (e.key === 'Enter') {
+					await sleep(0);
+					const values = group.value_partial;
+					submit.click?.(group);
+					prompt.content_close();
+					resolve(values.to_option());
+				}
+			},
+			() => elements[0].focus(),
+		);
+		main_panel_container
+			.create_panel(prompt, {
+				sizeable: false,
+				moveable: false,
+				modal: true,
+				width: 20,
+			})
+			.on_close()
+			.then(resolve);
+	});
 }
--- frontend/src/lib/prompts/shared.ts
+++ frontend/src/lib/prompts/shared.ts
@@ -1,28 +1,45 @@
-import { define_element } from "@chocbite/ts-lib-base";
-import { state, type StateROS } from "@chocbite/ts-lib-state";
-import { ContentBase } from "@libComposition";
+import { define_element } from '@chocbite/ts-lib-base';
+import { state, type StateROS } from '@chocbite/ts-lib-state';
+import { ContentBase } from '@libComposition';
 
 export class Prompt extends ContentBase {
-  static element_name() {
-    return "prompt";
-  }
-  static element_name_space() {
-    return "prompt";
-  }
+	static element_name() {
+		return 'prompt';
+	}
+	static element_name_space() {
+		return 'prompt';
+	}
 
-  get content_closable(): StateROS<boolean> {
-    return state.ok(true);
-  }
+	get content_closable(): StateROS<boolean> {
+		return state.ok(true);
+	}
 
-  #title;
-  get content_title(): StateROS<string> {
-    return this.#title;
-  }
+	#title;
+	get content_title(): StateROS<string> {
+		return this.#title;
+	}
 
-  constructor(title: string, element: HTMLElement) {
-    super();
-    this.#title = state.ok(title);
-    this.appendChild(element);
-  }
+	#onload?: () => void;
+
+	constructor(title: string, element: HTMLElement, key?: (e: KeyboardEvent) => void, onload?: () => void) {
+		super();
+		this.#title = state.ok(title);
+		this.appendChild(element);
+		this.addEventListener(
+			'keydown',
+			(e) => {
+				if (e.key === 'Escape') this.content_close();
+				key?.(e);
+			},
+			{ capture: true },
+		);
+		this.#onload = onload;
+	}
+
+	protected connectedCallback(): void {
+		super.connectedCallback();
+		this.focus();
+		this.#onload?.();
+	}
 }
 define_element(Prompt);
